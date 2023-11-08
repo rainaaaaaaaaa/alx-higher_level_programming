61f29cc25d8dff27a01972faa862b1cbe23c697f
Rain <rainymorning0000@gmail.com>
2023-11-08 08:14:58 -0800

    d

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,10 +2,8 @@
 def best_score(a_dictionary):
     max_key = None
     max_value = None
-
     for key, value in a_dictionary.items():
         if max_value is None or value > max_value:
             max_key = key
             max_value = value
-
     return max_key
