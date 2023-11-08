279fc78557ca9cabe56f1ac4e495a4a908e35e59
Rain <rainymorning0000@gmail.com>
2023-11-08 08:21:14 -0800

    d

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    return [num if not num == search else replace for num in my_list]
+    new_list = [replace if x == search else x for x in my_list]
+    return new_list
