40f656a64e74a396fd50acae5e435fbfba07efc2
Rain <rainymorning0000@gmail.com>
2023-11-08 16:53:46 -0800

    s

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
 if __name__ == "__main__":
-    from calculator_1 import add, subtract, multiply, divide
     a = 10
     b = 5
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, subtract(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, multiply(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, divide(a, b)))
+
+    add_result = add(a, b)
+    sub_result = sub(a, b)
+    mul_result = mul(a, b)
+    div_result = div(a, b)
+
+    print(f"{a} + {b} = {add_result}")
+    print(f"{a} - {b} = {sub_result}")
+    print(f"{a} * {b} = {mul_result}")
+    print(f"{a} / {b} = {div_result}")
