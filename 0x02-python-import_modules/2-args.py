40504abd75bb4f8c7a614647e9e7b6975df41cae
Rain <rainymorning0000@gmail.com>
2023-11-08 17:04:20 -0800

    s

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-if __name__ == "__main":
     argc = len(sys.argv) - 1
-
     if argc == 0:
         print("0 arguments.")
-    elif argc == 1:
-        print("1 argument:")
-    else:
-        print(f"{argc} arguments:")
+        sys.exit(0)
 
-    for i, arg in enumerate(sys.argv[1:], start=1):
-        print(f"{i}: {arg}")
+    ps = "s" if argc > 1 else ""
+    print("{} argument{}:".format(argc, ps))
+    i = 1
+    for arg in sys.argv[1:]:
+        print("{}: {}".format(i, arg))
+        i += 1
+else:
+    pass
