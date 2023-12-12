3ecf28a252beaf185e7d33fa44ef5099129d4af9
Rain <rainymorning0000@gmail.com>
2023-12-12 14:57:34 -0800

    l

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    my_list.sort(reverse = True)
-    for x in my_list:
-        print("{:d}".format(x))
+    if my_list:
+        my_list.sort(reverse=True)
+        for x in my_list:
+            print("{:d}".format(x))
