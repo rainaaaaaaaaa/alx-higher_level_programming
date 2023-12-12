d5add674b2418f6ad7441e7da976f57413f68503
Rain <rainymorning0000@gmail.com>
2023-12-11 17:16:11 -0800

    m

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     a = int(argv[1])
     b = int(argv[3])
     operator = argv[2]
+    """
     operator_functions = {
     '+': add,
     '-': sub,
@@ -17,6 +18,18 @@ if __name__ == "__main__":
     if operator in operator_functions:
         result = operator_functions[operator](a, b)
         print(f"{a} {operator} {b} = {result}")
+    """
+    if operator == '+':
+        print(f"{a} + {b} = {add(a, b)}")
+    elif operator == '-':
+        print(f"{a} - {b} = {sub(a, b)}")
+    elif operator == '*':
+        print(f"{a} * {b} = {mul(a, b)}")
+    elif operator == '/':
+        if b == 0:
+            print("Error: Division by zero is not allowed.")
+            exit(1)
+        print(f"{a} / {b} = {div(a, b)}")
     else:
         print("Unknown operator. Available operators: +, -, * and /", end="")
         exit(1)
