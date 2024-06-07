e43d188cf19879c24844a196ce67e68ff3d6c5d5
Rain <rainymorning0000@gmail.com>
2024-06-07 12:24:59 -0700

    sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
 -- Script to print the full description of the table first_table from the database hbtn_0c_0
 SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
 FROM information_schema.columns
-WHERE TABLE_SCHEMA = '[database_name]'
+WHERE TABLE_SCHEMA = 'hbtn_0c_0'
 AND TABLE_NAME = 'first_table';
